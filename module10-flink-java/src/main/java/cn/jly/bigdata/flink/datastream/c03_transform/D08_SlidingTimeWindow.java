c83f20395971e711f98ecf72b8a7930d9b0ed8d7
lanyangji <jly0823@163.com>
2021-07-13 16:56:34 +0800

    flink完善

--- module10-flink-java/src/main/java/cn/jly/bigdata/flink/datastream/c03_transform/D08_SlidingTimeWindow.java
+++ module10-flink-java/src/main/java/cn/jly/bigdata/flink/datastream/c03_transform/D08_SlidingTimeWindow.java
@@ -2,12 +2,22 @@ package cn.jly.bigdata.flink.datastream.c03_transform;
 
 import cn.jly.bigdata.flink.datastream.beans.SensorReading;
 import cn.jly.bigdata.flink.datastream.c02_source.D06_CustomRandomSource;
+import org.apache.commons.collections.IteratorUtils;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 import java.util.Arrays;
 
@@ -21,7 +31,7 @@ public class D08_SlidingTimeWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        env.addSource(new D06_CustomRandomSource.RandomSource(Arrays.asList("sensor-01",
+        WindowedStream<Tuple2<String, Double>, String, TimeWindow> windowDS = env.addSource(new D06_CustomRandomSource.RandomSource(Arrays.asList("sensor-01",
                 "sensor-02",
                 "sensor-03",
                 "sensor-04",
@@ -41,10 +51,95 @@ public class D08_SlidingTimeWindow {
                     }
                 })
                 // 每5秒触发统计过去15秒的数据
-                .window(SlidingProcessingTimeWindows.of(Time.seconds(15), Time.seconds(5)))
-                .maxBy(1)
-                .printToErr();
+                .window(SlidingProcessingTimeWindows.of(Time.seconds(15), Time.seconds(5)));
+
+        // 1. 增量聚合
+        // 允许迟到5秒钟，窗口触发的时候先输出一个计算结果，但是不关闭窗口，等再过一分钟后，将迟到的数据参与聚合后输出计算结果
+        // 来一条迟到数据，就在原来的窗口结果上聚合计算一次  -> 保证快速和计算结果的正确性
+        // 这种场景一般都是基于event time才有意义
+        OutputTag<Tuple2<String, Double>> lateOutputData = new OutputTag<>("late_date");
+        SingleOutputStreamOperator<Long> aggregateDS = windowDS.allowedLateness(Time.seconds(5))
+                .sideOutputLateData(lateOutputData) // 将迟到超过5秒的数据写入侧输出流
+                .aggregate(new CountAggregationFunction());
+
+        // 正常输出
+        aggregateDS.printToErr("count aggregate function");
+
+        // 拿到侧输出流中的迟到数据
+        DataStream<Tuple2<String, Double>> lateDs = aggregateDS.getSideOutput(lateOutputData);
+        lateDs.print();
+
+        // 2. 全窗口聚合
+        windowDS.apply(new MyWindowFunction())
+                .print("count window function");
 
         env.execute("D08_SlidingTimeWindow");
+    }
+
+    /**
+     * 自定义增量聚合函数
+     */
+    public static class CountAggregationFunction implements AggregateFunction<Tuple2<String, Double>, Long, Long> {
+
+        /**
+         * 创建累加器
+         *
+         * @return
+         */
+        @Override
+        public Long createAccumulator() {
+            return 0L;
+        }
+
+        /**
+         * 累加
+         *
+         * @param value
+         * @param accumulator
+         * @return
+         */
+        @Override
+        public Long add(Tuple2<String, Double> value, Long accumulator) {
+            return accumulator + 1L;
+        }
+
+        /**
+         * 获取累加器
+         *
+         * @param accumulator
+         * @return
+         */
+        @Override
+        public Long getResult(Long accumulator) {
+            return accumulator;
+        }
+
+        /**
+         * 合并累加器
+         *
+         * @param a
+         * @param b
+         * @return
+         */
+        @Override
+        public Long merge(Long a, Long b) {
+            return a + b;
+        }
+    }
+
+    /**
+     * 全窗口处理函数
+     * <p>
+     * 收集完窗口的所有数据
+     */
+    public static class MyWindowFunction implements WindowFunction<Tuple2<String, Double>, Tuple2<String, Long>, String, TimeWindow> {
+
+        @Override
+        public void apply(String key, TimeWindow window, Iterable<Tuple2<String, Double>> input, Collector<Tuple2<String, Long>> out) throws Exception {
+            long count = 0;
+            count += IteratorUtils.toList(input.iterator()).size();
+
+            out.collect(Tuple2.of(key, count));
+        }
     }
 }
--- module10-flink-java/src/main/java/cn/jly/bigdata/flink/datastream/c03_transform/D10_SessionWindow.java
+++ module10-flink-java/src/main/java/cn/jly/bigdata/flink/datastream/c03_transform/D10_SessionWindow.java
@@ -6,10 +6,16 @@ import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.OutputTag;
 
 import java.time.Duration;
 
@@ -19,9 +25,8 @@ import java.time.Duration;
  * @packageName cn.jly.bigdata.flink.datastream.c03_transform
  * @className D04_SessionWindow
  */
-public class D04_SessionWindow {
+public class D10_SessionWindow {
     public static void main(String[] args) throws Exception {
-
         ParameterTool tool = ParameterTool.fromArgs(args);
         String host = tool.get("host", "linux01");
         int port = tool.getInt("port", 9999);
@@ -29,7 +34,7 @@ public class D04_SessionWindow {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        env.socketTextStream(host, port)
+        WindowedStream<SensorReading, String, TimeWindow> windowDS = env.socketTextStream(host, port)
                 .map(new MapFunction<String, SensorReading>() {
                     @Override
                     public SensorReading map(String s) throws Exception {
@@ -52,10 +57,23 @@ public class D04_SessionWindow {
                         return sensorReading.getName();
                     }
                 })
-                .window(EventTimeSessionWindows.withGap(Time.milliseconds(3000)))
-                .maxBy("temperature")
-                .printToErr();
+                .window(EventTimeSessionWindows.withGap(Time.milliseconds(3000)));
 
-        env.execute("D04_SessionWindow");
+        // 允许迟到5秒钟，窗口触发的时候先输出一个计算结果，但是不关闭窗口，等再过一分钟后，将迟到的数据参与聚合后输出计算结果
+        // 来一条迟到数据，就在原来的窗口结果上聚合计算一次  -> 保证快速和计算结果的正确性
+        // 这种场景一般都是基于event time才有意义
+        OutputTag<SensorReading> lateOutputData = new OutputTag<>("late_date");
+        SingleOutputStreamOperator<SensorReading> maxDS = windowDS.allowedLateness(Time.seconds(5))
+                .sideOutputLateData(lateOutputData) // 将迟到超过5秒的数据写入侧输出流
+                .maxBy(1);
+
+        // 正常输出
+        maxDS.printToErr("count aggregate function");
+
+        // 拿到侧输出流中的迟到数据
+        DataStream<SensorReading> lateDs = maxDS.getSideOutput(lateOutputData);
+        lateDs.print();
+
+        env.execute("D10_SessionWindow");
     }
 }
