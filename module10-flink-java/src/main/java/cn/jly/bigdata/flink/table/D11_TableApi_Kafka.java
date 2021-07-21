4dfdc63d5c53f09c689e8d021621148d7458c6d1
lanyangji <jly0823@163.com>
2021-07-21 11:12:50 +0800

    table api更新

--- module10-flink-java/src/main/java/cn/jly/bigdata/flink/table/D11_TableApi_Kafka.java
+++ module10-flink-java/src/main/java/cn/jly/bigdata/flink/table/D11_TableApi_Kafka.java
@@ -10,14 +10,14 @@ import org.apache.flink.types.Row;
  * @date 2021/7/21 10:44
  */
 public class D11_TableApi_Kafka {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 1. 创建表环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
         // 2. 连接kafka
         String createKafkaTableSql =
-                "create table kafkaTable(`name` string, `temperature` double, `timestamp` bigint, `ts` timestamp(3) metadata from 'timestamp')" +
+                "create table kafkaTable(`name` string, `temperature` double, `event_time` bigint, `ts` timestamp(3) metadata from 'timestamp')" +
                         " with('connector' = 'kafka', 'topic' = 'sensor_reading', 'properties.bootstrap.servers' = 'linux01:9092'," +
                         " 'properties.group.id' = 'testGroup', 'scan.startup.mode' = 'earliest-offset', 'format' = 'json')";
         tableEnv.executeSql(createKafkaTableSql);
@@ -27,5 +27,7 @@ public class D11_TableApi_Kafka {
         kafkaTable.printSchema();
 
         tableEnv.toRetractStream(kafkaTable, Row.class).print();
+
+        env.execute("D11_TableApi_Kafka");
     }
 }
