9936362e264a8ba3faa603ed0af9a26408e2ebfc
lanyangji <jly0823@163.com>
2021-07-26 22:25:19 +0800

    sink to mysql

--- module10-flink-java/src/main/java/cn/jly/bigdata/flink_advanced/datastream/c04_sink/D07_CustomSink_MysqlSink.java
+++ module10-flink-java/src/main/java/cn/jly/bigdata/flink_advanced/datastream/c04_sink/D07_CustomSink_MysqlSink.java
@@ -1,8 +1,12 @@
 package cn.jly.bigdata.flink_advanced.datastream.c04_sink;
 
 import cn.jly.bigdata.flink_advanced.datastream.beans.TblUser;
+import cn.jly.bigdata.flink_advanced.utils.StreamWordCountUtils;
 import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 
@@ -22,6 +26,21 @@ public class D07_CustomSink_MysqlSink {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
 
+        DataStreamSource<String> fileDs = env.readTextFile("input/word.txt");
+        StreamWordCountUtils.wordCount(fileDs, "")
+                .map(new MapFunction<Tuple2<String, Long>, TblUser>() {
+                    @Override
+                    public TblUser map(Tuple2<String, Long> value) throws Exception {
+                        return new TblUser(value.f0, value.f1.intValue());
+                    }
+                })
+                .addSink(
+                        new MysqlSink(
+                                "jdbc:mysql://linux01:3306/test",
+                                "root",
+                                "123456"
+                        )
+                );
 
         env.execute("D07_CustomSink_MysqlSink");
     }
@@ -31,9 +50,9 @@ public class D07_CustomSink_MysqlSink {
      */
     public static class MysqlSink extends RichSinkFunction<TblUser> {
 
-        private String jdbcUrl;
-        private String user;
-        private String password;
+        private final String jdbcUrl;
+        private final String user;
+        private final String password;
 
         private Connection connection;
         private PreparedStatement preparedStatement;
@@ -46,16 +65,26 @@ public class D07_CustomSink_MysqlSink {
 
         @Override
         public void open(Configuration parameters) throws Exception {
-            connection = DriverManager.getConnection(jdbcUrl,user, password);
-
+            connection = DriverManager.getConnection(jdbcUrl, user, password);
+            String insertSql = "insert into tbl_user(name, age) values(?,?)";
+            preparedStatement = connection.prepareStatement(insertSql);
         }
 
         @Override
         public void close() throws Exception {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
         }
 
         @Override
         public void invoke(TblUser value, Context context) throws Exception {
+            preparedStatement.setString(1, value.getName());
+            preparedStatement.setInt(2, value.getAge());
+            preparedStatement.executeUpdate();
         }
     }
 }
---
+++ module10-flink-java/src/main/java/cn/jly/bigdata/flink_advanced/datastream/c05_connectors/D01_Connectors_Mysql.java
@@ -0,0 +1,15 @@
+package cn.jly.bigdata.flink_advanced.datastream.c05_connectors;
+
+/**
+ * flink提供的官方连接器 —— mysql
+ *
+ * @author jilanyang
+ * @package cn.jly.bigdata.flink_advanced.datastream.c05_connectors
+ * @class D01_Connectors_Mysql
+ * @date 2021/7/26 22:24
+ */
+public class D01_Connectors_Mysql {
+    public static void main(String[] args) {
+
+    }
+}
