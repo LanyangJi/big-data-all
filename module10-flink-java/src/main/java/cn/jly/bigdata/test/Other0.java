8cf673d28a83944fc28a25fe2a416ca72f0b6833
lanyangji <jly0823@163.com>
2021-09-17 22:52:31 +0800

    实现功能：获得指定类所在包及其子包下所有子类

---
+++ module10-flink-java/src/main/java/cn/jly/bigdata/test/Other0.java
@@ -0,0 +1,9 @@
+package cn.jly.bigdata.test;
+
+/**
+ * @author jilanyang
+ * @date 2021/9/17 19:34
+ */
+public class Other0 {
+
+}
---
+++ module10-flink-java/src/main/java/cn/jly/bigdata/test/c/C.java
@@ -0,0 +1,10 @@
+package cn.jly.bigdata.test.c;
+
+import cn.jly.bigdata.test.A;
+
+/**
+ * @author jilanyang
+ * @date 2021/9/17 19:08
+ */
+public class C extends A {
+}
